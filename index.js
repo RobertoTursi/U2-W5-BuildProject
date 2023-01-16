de30b869645c75dc57c8bdca2671b52749f6c4ef
RobertoTursi <Robertotursi00@gmail.com>
2023-01-16 16:28:18 +0100

    m

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+let headerHeight = document.querySelector(".start").offsetHeight - document.querySelector(".navigation").offsetHeight
+let nav = document.querySelector(".navigation")
+let button = document.getElementById("button")
+
+window.onscroll = () => {
+    if(window.scrollY > headerHeight) {
+        nav.classList.add("whiteNav")
+        button.classList.add("grennLi")
+    } else {
+        nav.classList.remove("whiteNav")
+        button.classList.remove("greenLi")
+    }
+}
+
 let lettera = document.querySelectorAll("svg g[stroke-linecap='butt']")
 
 let letteraAnimation = setInterval(() => {
@@ -9,3 +23,4 @@ let letteraAnimation = setInterval(() => {
         letteraRandom.style.opacity = 1
     }
 }, 100)
+
